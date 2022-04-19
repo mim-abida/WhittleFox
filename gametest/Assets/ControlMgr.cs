efc6f3683c5d4cd9c4d312c2716a47bab1186585
1001-Magaoay-Shantel <77915816+1001-Magaoay-Shantel@users.noreply.github.com>
2022-04-19 15:56:33 -0700

    alpha build
    :v

---
+++ gametest/Assets/ControlMgr.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMgr : MonoBehaviour
+{
+
+    public static ControlMgr inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    public float deltaVelocity = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        //controls
+
+        //left right movement
+
+       
+
+
+        //space for movement
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+        }
+        //s for crouch? maybe
+
+        //just for exiting the game 
+       /* if (Input.GetKey("escape"))
+        {
+            Application.Quit;
+        }*/
+    }
+}
---
+++ gametest/Assets/PlayerEntity.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private float verticalVelocity;
+    private float gravity = 14.0f;
+    private float jumpForce = 10.0f;
+
+    public int PlayerHealth = 100;
+    public float PlayerSpeed;
+
+    //colission stuff
+    public bool stopright;
+
+    private Rigidbody rig;
+
+    private void FixedUpdate()
+    {
+        stopright = Physics.Raycast(transform.position, Vector3.right, .67f);
+    }
+
+    void Start()
+    {
+        /* rig = GetComponent<Rigidbody>();*/
+        controller = GetComponent<CharacterController>();
+    }
+   
+
+     private void Update()
+    {
+        /*
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * PlayerSpeed * Time.deltaTime;
+
+
+        rig.MovePosition(transform.position + movement);
+        */
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * PlayerSpeed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = Input.GetAxis("Vertical") * PlayerSpeed;
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
